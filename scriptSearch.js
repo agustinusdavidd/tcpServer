8498d8f5ad90e300bb958f468d56df51a51413b5
CraftTex <nadinezuhdan@gmail.com>
2023-05-26 00:47:44 +0700

    Search function now works

--- scriptSearch.js
+++ scriptSearch.js
@@ -1,5 +1,29 @@
+const searchItem = document.URL
+    .split("=")[1]
+    .replaceAll('+',' ')
 
+document.getElementById("searchBar").value = decodeURIComponent(searchItem);
+
+const foodTemplate = document.getElementById("food-template");
+const foodContainer = document.getElementById("food-container");
 
 fetch("menu.json")
     .then(res => res.json())
-    .then(data => {})
+    .then(data => {
+        data.forEach(food => {
+                const card = foodTemplate.content.cloneNode(true);
+                const bgImg = card.getElementById("food-image-content");
+                const foodName = card.getElementById("food-name");
+                const foodLink = card.getElementById("food-button-link");
+                bgImg.src = food.image;
+                foodName.textContent = food.name;
+                foodLink.href = food.link;
+                const matched = food.name.toLowerCase().includes(searchItem.toLowerCase())
+                if (matched) {
+                    foodContainer.append(card);
+                };
+            }
+        )
+    }
+    );
+
